1676124888c019fddb39f916782592fd9bfcf417
Don Jayamanne <don.jayamanne@outlook.com>
2023-02-07 10:44:18 +1100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,55 @@
 import * as base from '@jupyter-widgets/base';
 import * as widgets from '@jupyter-widgets/controls';
 import * as outputWidgets from '@jupyter-widgets/jupyterlab-manager/lib/output';
-import * as embed from './embed';
 import { WidgetManager } from './manager';
 import './widgets.css';
 
-type KernelMessagingApi = {
-    postKernelMessage: (data: unknown) => void;
-};
+let loaded = false;
+function load() {
+    if (loaded) {
+        console.warn('Already loaded ipywidgets7');
+        return;
+    }
+    console.error('Initializing ipywidgets7');
+    // Export the following for `requirejs`.
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-empty,@typescript-eslint/no-empty-function
+    const define = (window as any).define || function () {};
+    define('@jupyter-widgets/controls', () => widgets);
+    define('@jupyter-widgets/base', () => base);
+    define('@jupyter-widgets/output', () => outputWidgets);
 
-// Default logger is console.log
-let logger = console.log;
+    // Create our window export. Necessary for the ipywidget code loading in the output to find our widget manager
+    // tslint:disable-next-line: no-any
+    (window as any).vscIPyWidgets = {
+        WidgetManager
+    };
+    loaded = true;
+}
+function unload() {
+    if (!loaded) {
+        return;
+    }
 
-// Export the following for `requirejs`.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, no-empty,@typescript-eslint/no-empty-function
-const define = (window as any).define || function () {};
-define('@jupyter-widgets/controls', () => widgets);
-define('@jupyter-widgets/base', () => base);
-define('@jupyter-widgets/output', () => outputWidgets);
-
-// Render existing widgets without a kernel and pull in the correct css files
-// This is not done yet. See this issue here: https://github.com/microsoft/vscode-python/issues/10794
-// Likely we'll do this in a different spot.
-if (document.readyState === 'complete') {
-    embed.renderWidgets(document.documentElement, logger);
-} else {
-    window.addEventListener('load', () => {
-        embed.renderWidgets(document.documentElement, logger);
-    });
+    try {
+        const undef = (window as any).undef || function () {};
+        undef('@jupyter-widgets/controls');
+        undef('@jupyter-widgets/base');
+        undef('@jupyter-widgets/output');
+        loaded = false;
+    } catch (e) {
+        console.warn(`Failed to unload IPYWidgets 8`, e);
+    }
 }
 
-// Create our window export. Necessary for the ipywidget code loading in the output to find our widget manager
-// tslint:disable-next-line: no-any
-(window as any).vscIPyWidgets = {
-    WidgetManager
-};
-
-// Has to be this form for VS code to load it correctly
-export function activate(context?: KernelMessagingApi) {
-    // Setup the logger function
-    if (context) {
-        logger = (msg) => {
-            context.postKernelMessage({
-                type: 'IPyWidgets_logMessage',
-                payload: msg
-            });
+console.error('Load ipywidgets7 Script file');
+export function activate() {
+    console.error('Activating ipywidgets7');
+    // Has to be this form for VS code to load it correctly
+    console.error('call activate in ipywidgets7');
+    if (!(window as any).vscIPyWidgets7) {
+        (window as any).vscIPyWidgets7 = {
+            load,
+            unload
         };
     }
 }
